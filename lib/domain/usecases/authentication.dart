64b97a21fd9fd7c206a84a6c2ba696e8786758d6
francostavares <francostavares2003@gmail.com>
2022-07-13 16:28:14 -0300

    muda argumento de auth() em Authentication

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,8 +1,15 @@
 import 'package:clean_architecture/domain/entities/account.dart';
 
 abstract class Authentication {
-  Future<Account> auth({
-    required String email,
-    required String password,
+  Future<Account> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({
+    required this.email,
+    required this.password,
   });
 }
