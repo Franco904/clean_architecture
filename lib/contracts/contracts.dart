b0b4afad4621ae8c2987c9fa3988ef5548661bc7
francostavares <francostavares2003@gmail.com>
2022-08-20 17:44:04 -0300

    Melhora organização de imports

--- lib/contracts/contracts.dart
+++ lib/contracts/contracts.dart
@@ -1,4 +1,3 @@
-export 'data/http/http_adapter.dart';
-export 'domain/usecases/authentication.dart';
-export 'presentation/login/login_presenter.dart';
-export 'presentation/protocols/validation.dart';
+export 'data/data.dart';
+export 'domain/domain.dart';
+export 'presentation/presentation.dart';
