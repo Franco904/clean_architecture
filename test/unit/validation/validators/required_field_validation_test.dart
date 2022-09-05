7fb80ad276b9344337be6c8b7a5c5f2b609cbd6b
francostavares <francostavares2003@gmail.com>
2022-09-05 15:42:43 -0300

    Remove imports desnecessários

--- test/unit/validation/validators/required_field_validation_test.dart
+++ test/unit/validation/validators/required_field_validation_test.dart
@@ -1,12 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 
-import 'package:clean_architecture/contracts/contracts.dart';
 import 'package:clean_architecture/validation/validation.dart';
-
-class MockValidation extends Mock implements Validation {}
-
-class MockAuthentication extends Mock implements Authentication {}
 
 void main() {
   group('RequiredFieldValidation | ', () {
